0c2cff0ffda311ba69a8c10d6f4996234dcd2d3d
Manuel Vera <manuelvera202@gmail.com>
2021-02-12 19:40:08 -0300

    change server

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const { sequelize } = require("./api/models");
 
 const app = express();
 
-var corsOption = {
-  origin: "http://localhost:8081",
-};
+// Loading routes
+const user_routes = require("./api/routes/user");
 
-// cors
-app.use(cors(corsOption));
-
-// parse request of application/json
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// parse reques of application/x-www-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// Cors
 
-// simple route
-
-app.get("/", (req, res) => {
-  res.json({ message: "Transaction app" });
-});
+// Routes
+app.use("/api", user_routes);
 
 // Server up
-app.listen({ port: 8081 }, async () => {
-  console.log(`Server up in http://localhost:8081`);
+app.listen({ port: 8080 }, async () => {
+  console.log(`Server up in http://localhost:8080`);
   await sequelize.authenticate({ force: true });
   console.log("DataBase Conected");
 });
