647551c415ba3faaa6c16797cbadfa1cb93d5296
Manuel Vera <manuelvera202@gmail.com>
2021-02-15 03:10:48 -0300

    modify api routes

--- backend/api/routes/user.js
+++ backend/api/routes/user.js
@@ -5,11 +5,11 @@ const { verifySignUp, authJwt } = require("../middleware");
 const api = express.Router();
 
 api.post(
-  "/user/signup",
+  "/signup",
   verifySignUp.checkDuplicateEmail,
   userController.createUser
 );
-api.post("/user/signin", userController.userSignin);
+api.post("/signin", userController.userSignin);
 api.get("/user/:id", authJwt.verifyToken, userController.getUser);
 api.put("/user/:id", authJwt.verifyToken, userController.updateUser);
 api.delete("/user/:id", authJwt.verifyToken, userController.deleteUser);
